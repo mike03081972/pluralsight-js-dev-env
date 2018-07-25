fd57cf0066ebf7870373e4b7b7233d78b269f6b3
Michael Marquet <m.marquet@evs.com>
2018-07-25 09:49:17 +0200

    ...

--- src/index.test.js
+++ src/index.test.js
@@ -13,7 +13,7 @@ describe('Our first test', () => {
 
 describe('index.html', () => {
 
-  it ('should say hello', (done) => {
+  it ('should have h1 that says Users', (done) => {
 
     const index = fs.readFileSync('./src/index.html', "utf-8");
 
@@ -21,7 +21,7 @@ describe('index.html', () => {
 
       const h1 = window.document.getElementsByTagName('h1')[0];
 
-      expect(h1.innerHTML).to.equal("Hello World !");
+      expect(h1.innerHTML).to.equal("Users");
 
       done();
 
